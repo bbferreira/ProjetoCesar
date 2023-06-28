23540685e2e3267c0e8459d3ce76e167348fc4ed
bbferreira <bruno@gmail.com>
2023-06-27 22:05:54 -0300

    Commit 22:05 27/06/2023 terminei html só está faltando alguns ajustes na qualidade das imagens

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,66 @@ const ImagensF = document.getElementById('imagens-container-2');
 
 const Promocao = document.getElementById('Promocao');
 const caminho = '';
+
+
+
+// teste
+
+document.addEventListener('DOMContentLoaded', function() {
+  const selectProduto = document.getElementById('select-produto');
+  const produtoImagem = document.getElementById('produto-imagem');
+  const btnAdicionar = document.getElementById('btn-adicionar');
+  const pedidoLista = document.getElementById('pedido-lista');
+  const pedidoForm = document.getElementById('pedido-form');
+
+  selectProduto.addEventListener('change', function() {
+    const produtoSelecionado = selectProduto.value;
+    const imagemUrl = `assets/img/perfumes masculinos/${produtoSelecionado}.jpg`;
+    produtoImagem.style.backgroundImage = `url(${imagemUrl})`;
+  });
+
+  btnAdicionar.addEventListener('click', function() {
+    const produtoSelecionado = selectProduto.value;
+    const produtoNome = selectProduto.options[selectProduto.selectedIndex].text;
+    const quantidade = document.getElementById('quantidade').value;
+
+    if (produtoSelecionado && quantidade > 0)
+    {
+      // Criar o elemento de item do pedido
+      const itemPedido = document.createElement('li');
+      itemPedido.innerHTML = `
+        <span>${produtoNome}</span>
+        <span>Quantidade: ${quantidade}</span>
+        <button class="btn-remover">Remover</button>
+      `;
+
+      // Adicionar evento de remoção ao botão
+      const btnRemover = itemPedido.querySelector('.btn-remover');
+      btnRemover.addEventListener('click', function() {
+        itemPedido.remove();
+      });
+
+      // Adicionar item do pedido à lista
+      pedidoLista.appendChild(itemPedido);
+
+      // Exibir o container do pedido
+      document.getElementById('pedido-container').style.display = 'block';
+    }
+  });
+
+  pedidoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const celular = document.getElementById('celular').value;
+
+    // Aqui você pode enviar os dados do pedido para o servidor ou realizar qualquer ação necessária com os dados.
+
+    // Limpar o formulário após o envio
+    pedidoForm.reset();
+    pedidoLista.innerHTML = '';
+    produtoImagem.style.backgroundImage = 'none';
+    document.getElementById('pedido-container').style.display = 'none';
+  });
+});
